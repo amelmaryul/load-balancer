7ebaed425205aeca76ee7d58c74a962bef11634b
Abdelmoez Elmaryul <am.elmaryul@gmail.com>
2026-08-23 22:39:37 +0200

    Add configuration

--- l7/main.go
+++ l7/main.go
@@ -1,77 +1,71 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"time"
 )
 
 var client = &http.Client{Timeout: 10 * time.Second}
 
-// i should have these configuered
-func getData(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	switch path {
-	case "/players":
-		resp, err := client.Get("http://localhost:61/api/players")
-		defer resp.Body.Close()
-		if err != nil {
-			fmt.Printf("Error at get request: %s\n", err.Error())
-			return
-		}
-
-		body, err := io.ReadAll(resp.Body)
-
-		if err != nil {
-			fmt.Printf("Error reading body: %s\n", err.Error())
-			return
-		}
-
-		w.Write([]byte(body))
-		fmt.Println(string(body))
-
-	case "/teams":
-		resp, err := client.Get("http://localhost:62/api/teams")
-		defer resp.Body.Close()
-		if err != nil {
-			fmt.Printf("Error at get request: %s\n", err.Error())
-			return
-		}
-		body, err := io.ReadAll(resp.Body)
-
-		if err != nil {
-			fmt.Printf("Error reading body: %s\n", err.Error())
-			return
-		}
-
-		w.Write([]byte(body))
-		fmt.Println(string(body))
-
-	case "/leagues":
-		resp, err := client.Get("http://localhost:63/api/leagues")
-		defer resp.Body.Close()
-		if err != nil {
-			fmt.Printf("Error at get request: %s\n", err.Error())
-			return
-		}
-		body, err := io.ReadAll(resp.Body)
-
-		if err != nil {
-			fmt.Printf("Error reading body: %s\n", err.Error())
-			return
-		}
-
-		w.Write([]byte(body))
-		fmt.Println(string(body))
-
-	default:
-		// return an error message
-		w.WriteHeader(404)
-	}
+type Route struct {
+	PathPrefx string `json:"pathprefix"`
+	Backend   string
 }
 
+type Config struct {
+	LbPort string
+	Routes []Route
+}
+
+func closure(config Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Inside handler")
+		path := r.URL.Path
+		for _, v := range config.Routes {
+			if path == v.PathPrefx {
+				resp, err := client.Get(v.Backend)
+				defer resp.Body.Close()
+				if err != nil {
+					fmt.Printf("Error at get request: %s\n", err.Error())
+					return
+				}
+				body, err := io.ReadAll(resp.Body)
+
+				if err != nil {
+					fmt.Printf("Error reading body: %s\n", err.Error())
+					return
+				}
+
+				w.Write([]byte(body))
+				fmt.Println(string(body))
+
+			}
+		}
+	}
+}
 func main() {
-	http.HandleFunc("/", getData)
+	b, err := os.ReadFile("./config.json")
+	if err != nil {
+		fmt.Printf("Error reading config file: %s\n", err.Error())
+		return
+	}
+	var config Config
+	err = json.Unmarshal(b, &config)
+	if err != nil {
+		fmt.Printf("Error unmarshaling: %s\n", err.Error())
+		return
+	}
+	handler := closure(config)
+	fmt.Println("What did i marshell???:")
+	fmt.Println(config.LbPort)
+	for _, v := range config.Routes {
+		fmt.Printf("prefix: %s   path: %s \n", v.PathPrefx, v.Backend)
+	}
+
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
